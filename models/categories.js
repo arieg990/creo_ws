4bbff259917999943883c6d83784a38466a85451
arieXeranta <arie@xeranta.com>
2020-04-11 19:39:56 +0700

    rename column category

--- models/categories.js
+++ models/categories.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const categories = sequelize.define('Category', {
-    category: {
+    name: {
       type:DataTypes.STRING,
     },
     imageUrl: DataTypes.STRING,
