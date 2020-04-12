a6ceb2be92297c30d3531aca40eee000de06b4ca
arieXeranta <arie@xeranta.com>
2020-04-12 13:45:13 +0700

    typo

--- models/vendors.js
+++ models/vendors.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
     imageUrl: DataTypes.STRING,
     url: DataTypes.STRING,
     rating: DataTypes.INTEGER,
-    isOfficial: DataTypes.isOfficial
+    isOfficial: DataTypes.BOOLEAN
   },{
     tableName: 'vendors',
     hooks: {
