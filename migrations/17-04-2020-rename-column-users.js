6eef997c5086201f5142ad87e31783ffe3bb3752
arieXeranta <arie@xeranta.com>
2020-04-18 00:01:32 +0700

    ketinggalan

--- migrations/17-04-2020-rename-column-users.js
+++ migrations/17-04-2020-rename-column-users.js
@@ -1,6 +1,15 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.renameColumn('users','role', 'roleId');
+    return queryInterface.addColumn('users','roleId', {
+    	type:Sequelize.STRING,
+    	references: {
+          model: 'roles', // name of Target model
+          key: 'role', // key in Target model that we're referencing
+        }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+  	return queryInterface.removeColumn('users','role');
   }
 };
