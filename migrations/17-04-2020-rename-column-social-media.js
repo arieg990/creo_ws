f5d293b887d150ada705cfde7b1bce9d0592df2d
arieXeranta <arie@xeranta.com>
2020-04-17 23:58:35 +0700

    no message

--- migrations/17-04-2020-rename-column-social-media.js
+++ migrations/17-04-2020-rename-column-social-media.js
@@ -1,6 +1,15 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.renameColumn('social_media','code', 'codeId');
+    return queryInterface.addColumn('social_media','codeId', {
+    	type:Sequelize.STRING,
+    	references: {
+          model: 'mst_codes', // name of Target model
+          key: 'code', // key in Target model that we're referencing
+        }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+  	return queryInterface.removeColumn('social_media','code');
   }
 };
--- migrations/17-04-2020-rename-column-vendor-users.js
+++ migrations/17-04-2020-rename-column-vendor-users.js
@@ -1,6 +1,15 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.renameColumn('vendor_users','role', 'roleId');
+    return queryInterface.addColumn('vendor_users','roleId', {
+    	type:Sequelize.STRING,
+    	references: {
+          model: 'roles', // name of Target model
+          key: 'role', // key in Target model that we're referencing
+        }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+  	return queryInterface.removeColumn('vendor_users','role');
   }
 };
